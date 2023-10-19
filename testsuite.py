fd921d8e0bd190ba864372adfe5cf5c9f6c9af27
nehadevaraj <ec22665@qmul.ac.uk>
2023-10-19 09:59:05 +0100

    all_docs

--- testsuite.py
+++ testsuite.py
@@ -1,3 +1,20 @@
-def add_numbers(a,b):
-    return a + b
-    #work
+import unittest
+
+from test_suite import add_numbers
+
+class TestFunction(unittest.TestCase):
+
+    def test_addition(self):
+        result = add_numbers(3,5)
+        self.assertEqual(result, 8)
+
+    def test_addition_negative_numbers(self):
+        result = add_numbers(-2, 7)
+        self.assertEqual(result, 5)
+
+    def test_addition_float_numbers(self):
+        result = add_numbers(1.5, 2.5)
+        self.assertAlmostEqual(result, 4.0, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
